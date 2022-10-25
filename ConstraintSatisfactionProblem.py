cb456f96477f3eeed6b10332b9b9220780dcff7c
Andy Le <andy.quile@gmail.com>
2022-10-25 09:45:29 -0700

    read_file method now has information about domain line and domains for each variable in the problem

--- ConstraintSatisfactionProblem.py
+++ ConstraintSatisfactionProblem.py
@@ -6,27 +6,61 @@
 # satisfaction problem.
 
 class CSP:
-	def __init__(self, number_of_var=0, domains=[], constraints=[]):
-		# attribute denoting the number of variables in the problem
-		self.num_var = number_of_var
-		# attribute denoting the list of domains, each with their 
-		# corresponding variable
-		self.domains = domains
-		# attribute denoting the list of constraints
-		# with each contraint being a tuple of 
-		# two items: a tuple of variables and 
-		# a relation that those variables take on
-		self.constraints = constraints
+    def __init__(self, number_of_var=0, domains=[], constraints=[]):
+        # attribute denoting the number of variables in the problem
+        self.num_var = number_of_var
+        # attribute denoting the list of domains, each with their 
+        # corresponding variable
+        self.domains = domains
+        # attribute denoting the list of constraints
+        # with each contraint being a tuple of 
+        # two items: a tuple of variables and 
+        # a relation that those variables take on
+        self.constraints = constraints
 
-	def read_file(self, filename):
-		# open filename given at the terminal
-		with open(filename, 'r') as file:
-			# read the first line of the file
-			first_line = file.readline()
-			# turn string into a list of numbers
-			nums_in_line = [ int(x) for x in first_line.strip().split(':') ]
-			print(nums_in_line)
-		    # read subsequent lines of file
-			for line in file:
-				print(line)
+    def read_file(self, filename):
+        # create a dictionary
+        # where key is the variable index
+        # and value is the domain (range)
+        domains_dict = {}
+        # open filename given at the terminal
+        with open(filename, 'r') as file:
+            # read the first line of the file
+            first_line = file.readline()
+            # turn string into a list of numbers
+            nums_in_line = [ int(x) for x in first_line.strip().split(':') ]
+            # Test output
+            print("Domain Line")
+            print(nums_in_line)
+            # read subsequent lines of file
+            for line in file:
+                # Assumption: every element in the 
+                # constraint line is separated
+                # by a whitespace
+                items = line.split()
+
+                indices = []
+                # Left hand side variable index
+                indices.append(int(items[2][1])) # e.g. take 0 from X0
+
+                # Check if we have right hand side variable
+                if items[6].startswith('X'):
+                    indices.append(int(items[6][1]))
+
+                for ind in indices:
+                    # check whether variable index 
+                    # is greater than the size
+                    # of nums_in_line
+                    if ind < len(nums_in_line) and ind != (len(nums_in_line) - 1):
+                        # create domain for variable with this above index
+                        domains_dict[ind] = list(range(nums_in_line[ind]))
+                    else:
+                        domains_dict[ind] = list(range(nums_in_line[-1]))
+
+        print("Domains Dictionary:")
+        print(domains_dict)
+
+
+                
+                
 

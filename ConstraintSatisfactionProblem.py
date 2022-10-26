ebea3c2048185436b53978419cb7e18d3bc45a2c
Andy Le <andy.quile@gmail.com>
2022-10-26 15:48:54 -0700

    read_file method now saves relevant information from problem into CSP class attributes

--- ConstraintSatisfactionProblem.py
+++ ConstraintSatisfactionProblem.py
@@ -6,23 +6,31 @@
 # satisfaction problem.
 
 class CSP:
-    def __init__(self, number_of_var=0, domains=[], constraints=[]):
+    def __init__(self, list_of_vars=[], domains={}, constraints=[]):
         # attribute denoting the number of variables in the problem
-        self.num_var = number_of_var
+        self._list_of_vars = list_of_vars
         # attribute denoting the list of domains, each with their 
         # corresponding variable
-        self.domains = domains
+        self._domains = domains
         # attribute denoting the list of constraints
         # with each contraint being a tuple of 
         # two items: a tuple of variables and 
         # a relation that those variables take on
-        self.constraints = constraints
-
+        self._constraints = constraints
+    
     def read_file(self, filename):
+        """This method helps process a text file
+           containing the syntax for a constraint
+           satisfaction problem. It will take
+           the relevant information regarding a CSP
+           and save them into the CSP class instance. 
+        """
+
         # create a dictionary
         # where key is the variable index
         # and value is the domain (range)
         domains_dict = {}
+
         # create a list of constraints
         # each contraint is represented 
         # as a tuple of 5 elements 
@@ -45,15 +53,19 @@ class CSP:
                 # constraint line is separated
                 # by one or more whitespaces
                 items = line.split()
-
+                
+                # a list of variable indices
                 indices = []
-                # Left hand side variable index
-                indices.append(int(items[2][1])) # e.g. take 0 from X0
+                # Left hand side variable index 
+                # (first encountered variable)
+                # below code takes 0 from X0 and add it to list
+                indices.append(int(items[2][1])) 
 
-                # Check if we have right hand side variable
+                # Check if we have a right hand side variable
                 if items[6].startswith('X'):
                     indices.append(int(items[6][1]))
 
+                # loop through the list of indices
                 for ind in indices:
                     # check whether variable index 
                     # is greater than the size
@@ -63,8 +75,17 @@ class CSP:
                         domains_dict[ind] = list(range(nums_in_line[ind]))
                     else:
                         domains_dict[ind] = list(range(nums_in_line[-1]))
-
+                # create a tuple for a constraint and save it into the list
                 constraints.append((items[0], items[2], items[4], items[5], items[6]))
+        
+        # Update CSP instance attributes
+        self._list_of_vars = sorted(domains_dict.keys())
+        self._domains = domains_dict
+        self._constraints = constraints
+
+        # Test output
+        print("Variables (Taken from Domains Dictionary)")
+        print(sorted(domains_dict.keys()))
         print("Domains Dictionary:")
         print(domains_dict)
         print("Constraints:")
